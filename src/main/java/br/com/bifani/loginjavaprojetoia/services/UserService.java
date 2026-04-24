d923c8abf6cb77bd2b9a2c4b405dda7911e5aac1
Bifaniii <gbifani.tech@gmail.com>
2026-04-24 12:42:25 -0300

    feat(UserService): creating UserService

---
+++ src/main/java/br/com/bifani/loginjavaprojetoia/services/UserService.java
@@ -0,0 +1,26 @@
+package br.com.bifani.loginjavaprojetoia.services;
+
+import br.com.bifani.loginjavaprojetoia.repositories.IUserRepository;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService implements UserDetailsService {
+    private final IUserRepository repository;
+    private final PasswordEncoder passwordEncoder;
+
+    public UserService(IUserRepository repository, PasswordEncoder passwordEncoder) {
+        this.repository = repository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        return repository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("Usuário com e-mail '" + email + "' não encontrado!"));
+    }
+}
