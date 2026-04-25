7db51cde3e9d05bb4cccd6258696c30004363e26
Bifani <gbifani.tech@gmail.com>
2026-04-24 23:37:10 -0300

    fix(repository): fix IHistoryRepository type parameters - Fix IHistoryRepository generic type to JpaRepository<History, Long> - Remove unnecessary Optional wrapper from findByUserId return type - Simplify repository interface for better consistency

--- src/main/java/br/com/bifani/loginjavaprojetoia/repositories/IHistoryRepository.java
+++ src/main/java/br/com/bifani/loginjavaprojetoia/repositories/IHistoryRepository.java
@@ -1,12 +1,11 @@
 package br.com.bifani.loginjavaprojetoia.repositories;
 
 import br.com.bifani.loginjavaprojetoia.model.History;
-import br.com.bifani.loginjavaprojetoia.model.User;
-import jakarta.persistence.Id;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.UUID;
 
-public interface IHistoryRepository extends JpaRepository<History, Id> {
-    List<History> findByUser(User user);
+public interface IHistoryRepository extends JpaRepository<History, Long> {
+    List<History> findByUserId(UUID userid);
 }
